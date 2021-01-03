ad75997892243f87d4bf3bc534b5bde4e15ac9b3
Drew Cutchins <drew.cutch@gmail.com>
2021-01-02 22:55:46 -0500

    Create ItemComponent

---
+++ SpellSword/Engine/Components/ItemComponent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GoRogue.GameFramework;
+using GoRogue.GameFramework.Components;
+using SpellSword.RPG.Items;
+
+namespace SpellSword.Engine.Components
+{
+    class ItemComponent: IGameObjectComponent
+    {
+        public IGameObject Parent { get; set; }
+
+        public Item Item { get; }
+
+        public ItemComponent(Item item)
+        {
+            Item = item;
+        }
+    }
+}
