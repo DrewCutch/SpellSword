87965486dfa163c25109171355d1313dd3cc0235
Drew Cutchins <drew.cutch@gmail.com>
2021-01-03 15:29:56 -0500

    Create Title

---
+++ SpellSword/Speech/Title.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpellSword.Speech
+{
+    class Title
+    {
+        public string Preposition { get; }
+        public string Article { get; }
+        public string Name { get; }
+
+        public Title(string preposition, string article, string name)
+        {
+            Preposition = preposition;
+            Article = article;
+            Name = name;
+        }
+
+        public Title(string article, string name)
+        {
+            Article = article;
+            Name = name;
+
+            Preposition = "on";
+        }
+
+        public override string ToString()
+        {
+            return Article + " " + Name;
+        }
+    }
+}
