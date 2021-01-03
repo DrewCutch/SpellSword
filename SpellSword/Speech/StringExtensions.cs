e680ef6c15c41290b11d0da1a29b0b68f5ef8c2e
Drew Cutchins <drew.cutch@gmail.com>
2021-01-03 15:38:23 -0500

    Add Docstring to StringExtensions.WithTrailingSpace

--- SpellSword/Speech/StringExtensions.cs
+++ SpellSword/Speech/StringExtensions.cs
@@ -7,6 +7,11 @@ namespace SpellSword.Speech
 {
     static class StringExtensions
     {
+        /// <summary>
+        /// Returns the string with a trailing space if it is not null or empty
+        /// </summary>
+        /// <param name="str">The string to append a trailing space to</param>
+        /// <returns>The string with a trailing space</returns>
         public static string WithTrailingSpace(this string str)
         {
             if (string.IsNullOrEmpty(str))
