0c9e5ed596a0b100a459caae0b7a6b6e03db81d9
Drew Cutchins <drew.cutch@gmail.com>
2021-01-02 22:52:20 -0500

    Create Item

---
+++ SpellSword/RPG/Items/Item.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using SpellSword.Render;
+
+namespace SpellSword.RPG.Items
+{
+    abstract class Item
+    {
+        private static readonly string[] RarityTitles = new[] {"Trash", "Common", "Uncommon", "Rare", "Epic", "Legendary"};
+
+        public bool CanBeEquipped => SlotKind != EquipmentSlotKind.None;
+        public EquipmentSlotKind SlotKind { get; }
+        public string Name { get; }
+        public string Description { get; }
+        public Glyph Glyph { get; }
+        public int Rarity { get; }
+
+        public string RarityTitle => Rarity < RarityTitles.Length ? RarityTitles[Rarity] : RarityTitles.Last();
+
+        public Item(string name, string description, Glyph glyph, EquipmentSlotKind slotKind, int rarity)
+        {
+            Name = name;
+            Description = description;
+            Glyph = glyph;
+            SlotKind = slotKind;
+            Rarity = rarity;
+        }
+    }
+}
