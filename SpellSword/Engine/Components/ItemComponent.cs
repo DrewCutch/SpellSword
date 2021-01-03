e7447afbb9f56ddff0cbf734bde50dc8e6904c98
Drew Cutchins <drew.cutch@gmail.com>
2021-01-03 15:34:00 -0500

    Update ItemComponent to implement IInteractable for item pick ups

--- SpellSword/Engine/Components/ItemComponent.cs
+++ SpellSword/Engine/Components/ItemComponent.cs
@@ -1,19 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
+using GoRogue;
 using GoRogue.GameFramework;
 using GoRogue.GameFramework.Components;
+using SpellSword.Actors;
+using SpellSword.Actors.Action;
+using SpellSword.Logging;
 using SpellSword.RPG.Items;
+using SpellSword.Speech;
+using SpellSword.Time;
 
 namespace SpellSword.Engine.Components
 {
-    class ItemComponent: Component
+    class ItemComponent: Component, IInteractable
     {
         public Item Item { get; }
+        public int Range => 1;
+        public Distance RangeDistanceType => Distance.MANHATTAN;
 
         public ItemComponent(Item item)
         {
             Item = item;
+        }
+
+        public void Interact(Actor by)
+        {
+            Parent.CurrentMap.RemoveEntity(Parent);
+            by.Being.Inventory.Add(Item);
+
+            by.MainBus.Send(new LogMessage($"{{0}} picked up {Item.Title.Article.WithTrailingSpace()}{Item.Title.Name}", new LogLink(by.Being.Name, Color.Aquamarine, by)));
+        }
+
+        public EventTimerTiming InteractTiming(Actor by)
+        {
+            return new EventTimerTiming(100,0);
         }
     }
 }
