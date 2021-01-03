784ad67f35ca82c20420a1327b95583467c40062
Drew Cutchins <drew.cutch@gmail.com>
2021-01-03 15:36:56 -0500

    Create StringExtensions with WithTrailingSpace method

---
+++ SpellSword/Speech/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SpellSword.Speech
+{
+    static class StringExtensions
+    {
+        public static string WithTrailingSpace(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return "";
+
+            return str + " ";
+        }
+    }
+}
