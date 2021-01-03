9891eea6791048e25bb6b48c0e0d6057f9b31fec
Drew Cutchins <drew.cutch@gmail.com>
2021-01-02 22:52:52 -0500

    Create MapExtensions with IsWalkable extension method

---
+++ SpellSword/MapGeneration/MapExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GoRogue;
+using GoRogue.GameFramework;
+
+namespace SpellSword.MapGeneration
+{
+    static class MapExtensions
+    {
+        public static bool IsWalkable(this Map map, Coord pos)
+        {
+            return map.WalkabilityView[pos];
+        }
+    }
+}
