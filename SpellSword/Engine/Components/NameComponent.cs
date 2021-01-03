b1f21fc1c7fb23666e2d0cd83752123a578ebcde
Drew Cutchins <drew.cutch@gmail.com>
2021-01-02 22:49:16 -0500

    Create NameComponent and Describer

---
+++ SpellSword/Engine/Components/NameComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GoRogue.GameFramework;
+using GoRogue.GameFramework.Components;
+
+namespace SpellSword.Engine.Components
+{
+    class NameComponent: IGameObjectComponent
+    {
+        public IGameObject Parent { get; set; }
+
+        public string Name { get; }
+
+        public NameComponent(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ SpellSword/Engine/Describer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GoRogue.GameFramework;
+using GoRogue.MapViews;
+using GoRogue.Messaging;
+using SpellSword.Engine.Components;
+using SpellSword.Input;
+using SpellSword.Render.Panes;
+
+namespace SpellSword.Engine
+{
+    class Describer : ISubscriber<MouseMoveEvent>
+    {
+        public Map Map { get; }
+
+        private readonly TextPane _descriptionPane;
+
+        public Describer(TextPane descriptionPane, Map map)
+        {
+            _descriptionPane = descriptionPane;
+            Map = map;
+        }
+
+        public void Handle(MouseMoveEvent message)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            if (!Map.Contains(message.NewPos))
+                return;
+
+            bool first = true;
+
+            foreach (IGameObject gameObject in Map.GetObjects(message.NewPos))
+            {
+                if (gameObject.GetComponent<NameComponent>() is NameComponent nameComponent)
+                {
+                    if (!first)
+                    {
+                        builder.Append(" on ");
+                    }
+
+                    builder.Append(nameComponent.Name);
+                    first = false;
+                }
+            }
+
+            _descriptionPane.Text = builder.ToString();
+        }
+    }
+}
