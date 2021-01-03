9a0898f997db56ab064ca162f0e399c8aac21fe1
Drew Cutchins <drew.cutch@gmail.com>
2021-01-03 15:32:18 -0500

    Implement IInteractable and InteractAction

---
+++ SpellSword/Actors/Action/IInteractable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GoRogue;
+using SpellSword.Time;
+
+namespace SpellSword.Actors.Action
+{
+    interface IInteractable
+    {
+        void Interact(Actor by);
+
+        public int Range { get; }
+
+        public Distance RangeDistanceType { get; }
+
+        public EventTimerTiming InteractTiming(Actor by);
+    }
+}
---
+++ SpellSword/Actors/Action/InteractAction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SpellSword.Time;
+
+namespace SpellSword.Actors.Action
+{
+    class InteractAction: ActorAction
+    {
+        private readonly IInteractable _interactable;
+
+        public InteractAction(Actor actor, IInteractable interactable) : base(actor, new EventTimer(interactable.InteractTiming(actor)))
+        {
+            _interactable = interactable;
+        }
+
+        public override bool Resolve()
+        {
+            if (Timer.PendingMoments.Contains(EventTimer.EventTimerMoment.Action))
+                _interactable.Interact(Actor);
+
+            return Timer.PendingMoments.Contains(EventTimer.EventTimerMoment.End);
+        }
+    }
+}
