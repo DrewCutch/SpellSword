c15d6691711ea0911d8a6395126ce8de6e2c2ba8
Drew Cutchins <drew.cutch@gmail.com>
2021-01-04 11:56:31 -0500

    Update Inventory to handle item duplicates

--- SpellSword/RPG/Items/Inventory.cs
+++ SpellSword/RPG/Items/Inventory.cs
@@ -6,27 +6,38 @@ namespace SpellSword.RPG.Items
 {
     class Inventory
     {
-        public IReadOnlyList<Item> Items => _items;
-        private List<Item> _items;
+        public IReadOnlyDictionary<Item, int> Items => _items;
+        private SortedDictionary<Item, int> _items;
 
         public event Action<Item> OnAdd;
         public event Action<Item> OnRemove;
 
         public Inventory()
         {
-            _items = new List<Item>();
+            _items = new SortedDictionary<Item, int>();
         }
 
         public void Add(Item item)
         {
-            _items.Add(item);
+            Add(item, 1);
+        }
+
+        public void Add(Item item, int amount)
+        {
+            int currentAmount = _items.GetValueOrDefault(item, 0);
+            _items[item] = currentAmount + amount;
 
             OnAdd?.Invoke(item);
         }
 
         public void Remove(Item item)
         {
-            _items.Remove(item);
+            Remove(item, 1);
+        }
+
+        public void Remove(Item item, int amount)
+        {
+            _items[item] -= amount;
 
             OnRemove?.Invoke(item);
         }
