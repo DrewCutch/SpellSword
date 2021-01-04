0439b6477327dc6ac8558cba94f8d067a105f7cb
Drew Cutchins <drew.cutch@gmail.com>
2021-01-04 11:51:34 -0500

    Create IEnumerableExtensions with WithIndex and AtIndex methods

---
+++ SpellSword/Util/IEnumerableExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpellSword.Util
+{
+    static class IEnumerableExtensions
+    {
+
+        public static IEnumerable<(T, int)> WithIndex<T>(this IEnumerable<T> enumerable)
+        {
+            int i = 0;
+            foreach (T value in enumerable)
+            {
+                yield return (value, i);
+
+                i += 1;
+            }
+        }
+
+        public static T AtIndex<T>(this IEnumerable<T> enumerable, int index)
+        {
+            int i = 0;
+            foreach (T value in enumerable)
+            {
+                if (i == index)
+                    return value;
+
+                i += 1;
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+    }
+}
