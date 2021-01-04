8243ac3eafecbcbafcc3248d59d6e3316c8baf4a
Drew Cutchins <drew.cutch@gmail.com>
2021-01-04 11:55:00 -0500

    Refactor BearTermRenderer to use a collection of Windows
    This refactor opens the possibility for a windowing system where each window can utilize separate terminal layers

--- SpellSword/Render/BearTermRenderer.cs
+++ SpellSword/Render/BearTermRenderer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using BearLib;
 using GoRogue.Messaging;
 using SpellSword.Engine;
@@ -11,156 +12,58 @@ namespace SpellSword.Render
 {
     class BearTermRenderer: ISubscriber<ParticleEvent>
     {
+        private Dictionary<Pane, Window> _windows;
         private Pane _root;
-        private TerminalWritable _texture;
-        private TerminalWritable _particleWritable;
 
-        private HashSet<IParticleEffect> _particleEffects;
-
-        private long _lastFrame;
-
-        private bool _particleEffectRemoved;
-
-        public BearTermRenderer(Pane root, string options = "")
+        public BearTermRenderer(Window root, string options = "")
         {
-            _root = root;
-
-            if(!Terminal.Open())
+            if (!Terminal.Open())
                 throw new Exception("Terminal instance is already open!");
 
             Terminal.Set(options);
 
-            _texture = new TerminalWritable(Terminal.State(Terminal.TK_WIDTH), Terminal.State(Terminal.TK_HEIGHT), 0);
-            _particleWritable = new TerminalWritable(Terminal.State(Terminal.TK_WIDTH), Terminal.State(Terminal.TK_HEIGHT), 1);
-
-            _lastFrame = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
-            _particleEffects = new HashSet<IParticleEffect>();
-            _particleEffectRemoved = false;
-
             Terminal.Refresh();
+
+            _root = root.Root;
+
+            _windows = new Dictionary<Pane, Window>();
+            _windows[_root] = root;
         }
 
         public void Refresh()
         {
-            bool particlesUpdated = false;
-
-            long now = Environment.TickCount;
-            long deltaTime = now - _lastFrame;
-
-            _root.SuggestWidth(_texture.Width);
-            _root.SuggestHeight(_texture.Height);
-
-            _root.Paint(_texture);
-
-            foreach (IParticleEffect particleEffect in _particleEffects)
-            {
-                particleEffect.Update(deltaTime);
-                particlesUpdated |= particleEffect.Changed;
-            }
-
-            _particleEffects.RemoveWhere(effect => effect.Finished);
-
-            if (particlesUpdated || _particleEffectRemoved)
-                PaintParticles();
-
-            if (_particleWritable.Dirty)
-            {
-                Terminal.Refresh();
-                _particleWritable.Clean();
-            }
-
-            if (_texture.Dirty)
-            {
-                Terminal.Refresh();
-                _texture.Clean();
-            }
-
-            _particleEffectRemoved = false;
-            _lastFrame = now;
-        }
-
-        private void PaintParticles()
-        {
-            ((IWriteable)_particleWritable).Clear();
-
-            foreach (IParticleEffect effect in _particleEffects)
-            {
-                foreach (Particle particle in effect.Particles())
-                {
-                    _particleWritable.WriteGlyph(particle.Pos.Y + 1, particle.Pos.X, particle.Glyph);
-                }
-            }
-        }
-
-        private void ClearCurrentLayer()
-        {
-            for (int x = 0; x < _texture.Width; x++)
-            {
-                for (int y = 0; y < _texture.Height; y++)
-                {
-                    Terminal.Put(x, y, ' ');
-                }
-            }
-        }
-
-        /*public void Paint(TextTexture texture)
-        {
             bool dirty = false;
-            bool compositing = false;
-
-            foreach (var (layerType, layer) in texture.DirtyLayers())
+            foreach (Window window in _windows.Values)
             {
-                Terminal.Layer(layerType.Num);
+                window.Refresh();
 
-                for (int x = 0; x < texture.Width; x++)
-                {
-                    for (int y = 0; y < texture.Height; y++)
-                    {
-                        if (layer[y][x] == null)
-                            continue;
+                dirty |= window.Dirty;
 
-                        if (layer[y][x].BackgroundColor is Color backgroundColor)
-                        {
-                            Terminal.Color(backgroundColor);
-                            Terminal.Put(x, y, '█');
-                            Terminal.Composition(true);
-                            compositing = true;
-                        }
-
-                        Terminal.Color(layer[y][x].Color);
-
-                        Terminal.Put(x, y, layer[y][x].Character);
-                        if(compositing)
-                            Terminal.Composition(false);
-                    }
-                }
-
-                dirty = true;
+                window.Clean();
             }
 
-            if(dirty)
+            if (dirty)
                 Terminal.Refresh();
-
-            texture.Clean();
-        }*/
-
-        public void AddEffect(IParticleEffect effect)
-        {
-            _particleEffects.Add(effect);
         }
 
-        public void RemoveEffect(IParticleEffect effect)
-        {
-            _particleEffects.Remove(effect);
-            _particleEffectRemoved = true;
-        }
 
         public void Handle(ParticleEvent message)
         {
-            if (message.Start)
-                AddEffect(message.Effect);
+            _windows[_root].Handle(message);
+        }
+
+        public void Handle(WindowEvent windowEvent)
+        {
+            if (windowEvent.OpenEvent)
+            {
+                Window newWindow = new Window(windowEvent.Root, windowEvent.Bounds, _windows.Count);
+
+                _windows[windowEvent.Root] = newWindow;
+            }
             else
-                RemoveEffect(message.Effect);
+            {
+                _windows.Remove(windowEvent.Root);
+            }
         }
     }
 }
---
+++ SpellSword/Render/Window.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BearLib;
+using GoRogue;
+using SpellSword.Input;
+using SpellSword.Render.Panes;
+using SpellSword.Render.Particles;
+
+namespace SpellSword.Render
+{
+    class Window
+    {
+        public Pane Root { get; }
+        public int ZIndex { get; }
+        public bool Dirty { get; private set; }
+
+        private TerminalWritable _writable;
+        private TerminalWritable _particleWritable;
+
+        private HashSet<IParticleEffect> _particleEffects;
+
+        private long _lastFrame;
+        private bool _particleEffectRemoved;
+
+        public Window(Pane root, Rectangle bounds, int zIndex)
+        {
+            Root = root;
+            ZIndex = zIndex;
+
+            _writable = new TerminalWritable(bounds.Width, bounds.Height, zIndex * 2);
+            _particleWritable = new TerminalWritable(bounds.Width, bounds.Height, zIndex * 2 + 1);
+
+            _lastFrame = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            _particleEffects = new HashSet<IParticleEffect>();
+            _particleEffectRemoved = false;
+
+            Dirty = true;
+        }
+
+        public void Refresh()
+        {
+            bool particlesUpdated = false;
+
+            long now = Environment.TickCount;
+            long deltaTime = now - _lastFrame;
+
+            Root.SuggestWidth(_writable.Width);
+            Root.SuggestHeight(_writable.Height);
+
+            Root.Paint(_writable);
+
+            foreach (IParticleEffect particleEffect in _particleEffects)
+            {
+                particleEffect.Update(deltaTime);
+                particlesUpdated |= particleEffect.Changed;
+            }
+
+            _particleEffects.RemoveWhere(effect => effect.Finished);
+
+            if (particlesUpdated || _particleEffectRemoved)
+                PaintParticles();
+
+            Dirty = _particleWritable.Dirty || _writable.Dirty;
+
+            _particleEffectRemoved = false;
+            _lastFrame = now;
+        }
+
+        private void PaintParticles()
+        {
+            ((IWriteable)_particleWritable).Clear();
+
+            foreach (IParticleEffect effect in _particleEffects)
+            {
+                foreach (Particle particle in effect.Particles())
+                {
+                    _particleWritable.WriteGlyph(particle.Pos.Y + 1, particle.Pos.X, particle.Glyph);
+                }
+            }
+        }
+
+        public void Clean()
+        {
+            _writable.Clean();
+            _particleWritable.Clean();
+            Dirty = false;
+        }
+
+        public void AddEffect(IParticleEffect effect)
+        {
+            _particleEffects.Add(effect);
+        }
+
+        public void RemoveEffect(IParticleEffect effect)
+        {
+            _particleEffects.Remove(effect);
+            _particleEffectRemoved = true;
+        }
+
+        public void Handle(ParticleEvent message)
+        {
+            if (message.Start)
+                AddEffect(message.Effect);
+            else
+                RemoveEffect(message.Effect);
+        }
+    }
+}
