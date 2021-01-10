a1586c7917435a3ef7627f687f11867e3c88d79b
Drew Cutchins <drew.cutch@gmail.com>
2021-01-09 19:53:54 -0500

    Update InventoryDisplayPane to use internal stack view

--- SpellSword/Render/Panes/InventoryDisplayPane.cs
+++ SpellSword/Render/Panes/InventoryDisplayPane.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Text;
 using BearLib;
 using GoRogue;
+using GoRogue.Messaging;
 using SpellSword.Actors;
+using SpellSword.Engine;
 using SpellSword.Input;
 using SpellSword.RPG;
 using SpellSword.RPG.Items;
@@ -15,189 +17,302 @@ namespace SpellSword.Render.Panes
 {
     class InventoryDisplayPane: Pane
     {
-        public EquipmentSlotSet EquipmentSlotSet { get; }
-        public Inventory Inventory { get; }
+        private readonly StackPane _stackPane;
+        private readonly EquipmentSlotPane _equipmentSlotPane;
+        private readonly InventoryPane _inventoryPane;
+        private readonly MessageBus _mainBus;
 
         private readonly JoystickConfig _joystickConfig;
 
-        private int _inventoryIndex;
-        private int _equipmentIndex;
-
-        public InventoryDisplayPane(EquipmentSlotSet equipmentSlotSet, Inventory inventory, JoystickConfig joystickConfig)
+        public override bool Dirty
         {
-            EquipmentSlotSet = equipmentSlotSet;
-            Inventory = inventory;
+            get => _stackPane.Dirty;
+            protected set { }
+        }
+
+
+        public InventoryDisplayPane(EquipmentSlotSet equipmentSlotSet, Inventory inventory,
+            JoystickConfig joystickConfig, MessageBus mainBus)
+        {
+            _equipmentSlotPane = new EquipmentSlotPane(equipmentSlotSet);
+            _inventoryPane = new InventoryPane(inventory);
+
+            _stackPane = new StackPane(StackPane.StackDirection.Horizontal);
+            _stackPane.AddChild(_equipmentSlotPane, 1);
+            _stackPane.AddChild(_inventoryPane, 1);
+
+            _mainBus = mainBus;
 
             _joystickConfig = joystickConfig;
-            _equipmentIndex = -1;
-
-            Dirty = true;
-
-            EquipmentSlotSet.OnEquipped += OnEquipped;
-            Inventory.OnAdd += InventoryOnAdd;
-            Inventory.OnRemove += InventoryOnRemove;
-
-            _inventoryIndex = 0;
         }
 
-        private void InventoryOnAdd(Item obj)
+
+        public override void SuggestHeight(int height)
         {
-            Dirty = true;
+            base.SuggestHeight(height);
+            _stackPane.SuggestHeight(height);
         }
 
-        private void InventoryOnRemove(Item obj)
+        public override void SuggestWidth(int width)
         {
-            Dirty = true;
-        }
-
-        private void OnEquipped(EquipmentSlot arg1, Item arg2)
-        {
-            Dirty = true;
+            base.SuggestWidth(width);
+            _stackPane.SuggestWidth(width);
         }
 
         public override bool Paint(IWriteable writeContext)
         {
-            if (!Dirty)
-                return false;
-
-            Coord nextMessage = new Coord(0,0);
-            writeContext.Clear();
-
-            Coord equippedLineEnd = StringPrinter.Print("Equipped:", writeContext, nextMessage.X, nextMessage.Y);
-            nextMessage = new Coord(0, equippedLineEnd.Y + 1);
-
-            foreach ((EquipmentSlot equipmentSlot, int i) in EquipmentSlotSet.Slots.WithIndex())
-            {
-                bool slotSelected = HasFocus && _equipmentIndex == i;
-                Color textColor = slotSelected ? Color.Black : Color.White;
-                Color backgroundColor = slotSelected ? Color.White : Color.Black;
-
-                Coord lineEnd = StringPrinter.Print($"{equipmentSlot.Name}: {EquipmentSlotSet.Equipped.GetValueOrDefault(equipmentSlot)?.Title.Name ?? "Nothing"}", writeContext, textColor, backgroundColor, nextMessage.X, nextMessage.Y); 
-
-                nextMessage = new Coord(0, lineEnd.Y + 1);
-            }
-
-            Coord inventoryLineEnd = StringPrinter.Print("Inventory:", writeContext, nextMessage.X, nextMessage.Y);
-            nextMessage = new Coord(0, inventoryLineEnd.Y + 1);
-
-            foreach ((Item item, int i) in Inventory.Items.Keys.WithIndex())
-            {
-                bool itemSelected = HasFocus && i == _inventoryIndex;
-                Color textColor = itemSelected ? Color.Black : Color.White;
-                Color backgroundColor = itemSelected ? Color.White : Color.Black;
-
-                Coord lineEnd = StringPrinter.Print($"{item.Title.Name} (x{Inventory.Items[item]})", writeContext, textColor, backgroundColor, nextMessage.X, nextMessage.Y);
-
-                nextMessage = new Coord(0, lineEnd.Y + 1);
-            }
-
-            Dirty = false;
-
-            return true;
+            return _stackPane.Paint(writeContext);
         }
 
         public override void OnKeyDown(int keyCode)
         {
             if (keyCode == _joystickConfig.DownCode)
             {
-                if(_equipmentIndex != -1)
-                    IncrementEquipmentIndex();
+                if(_equipmentSlotPane.CurrentlyEquipping)
+                    _equipmentSlotPane.IncrementEquipmentIndex();
                 else
-                    _inventoryIndex = Math.Min(_inventoryIndex + 1, Inventory.Items.Count - 1);
+                    _inventoryPane.IncrementSelectedItemIndex();
+                    
             }
-
-            if (keyCode == _joystickConfig.UpCode)
+            else if (keyCode == _joystickConfig.UpCode)
             {
-                if (_equipmentIndex != -1)
-                    DecrementEquipmentIndex();
+                if (_equipmentSlotPane.CurrentlyEquipping)
+                    _equipmentSlotPane.DecrementEquipmentIndex();
                 else
-                    _inventoryIndex = Math.Max(_inventoryIndex - 1, 0);
+                    _inventoryPane.DecrementSelectedItemIndex();
             }
-
-            if (keyCode == Terminal.TK_E)
+            else if (keyCode == _joystickConfig.Equip)
             {
-                if (_equipmentIndex != -1)
+                if (_equipmentSlotPane.CurrentlyEquipping)
                     Equip();
                 else
-                    StartEquipping();
+                    _equipmentSlotPane.BeginEquipping(_inventoryPane.CurrentlySelectedItem);
             }
-
-            Dirty = true;
-        }
-
-        private void StartEquipping()
-        {
-            if (!Inventory.Items.Keys.AtIndex(_inventoryIndex).CanBeEquipped)
-                return;
-
-            _equipmentIndex = -1;
-            IncrementEquipmentIndex();
-
-            Dirty = true;
         }
 
         private void Equip()
         {
-            Item itemToEquip = Inventory.Items.Keys.AtIndex(_inventoryIndex);
-            Inventory.Remove(itemToEquip);
+            Item itemToEquip = _inventoryPane.CurrentlySelectedItem;
+            _inventoryPane.Inventory.Remove(itemToEquip);
 
-            EquipmentSlot slot = EquipmentSlotSet.Slots[_equipmentIndex];
+            EquipmentSlot slot = _equipmentSlotPane.GetCurrentlySelectedSlot();
 
-            Item replacedItem = EquipmentSlotSet.Equip(itemToEquip, slot);
+            Item replacedItem = _equipmentSlotPane.EquipmentSlotSet.Equip(itemToEquip, slot);
 
-            if(replacedItem != null)
-                Inventory.Add(replacedItem);
+            if (replacedItem != null)
+                _inventoryPane.Inventory.Add(replacedItem);
 
-            _equipmentIndex = -1;
-        }
-
-        private void IncrementEquipmentIndex()
-        {
-            EquipmentSlotKind slotKind = Inventory.Items.Keys.AtIndex(_inventoryIndex).SlotKind;
-            int i = (_equipmentIndex + 1) % EquipmentSlotSet.Slots.Count;
-
-            while (i != _equipmentIndex)
-            {
-                if (EquipmentSlotSet.Slots[i].Kind == slotKind)
-                    break;
-
-                i = (i + 1) % EquipmentSlotSet.Slots.Count;
-            }
-
-            _equipmentIndex = i;
-        }
-
-        private void DecrementEquipmentIndex()
-        {
-            EquipmentSlotKind slotKind = Inventory.Items.Keys.AtIndex(_inventoryIndex).SlotKind;
-            int i = _equipmentIndex == 0 ? EquipmentSlotSet.Slots.Count - 1 : _equipmentIndex - 1;
-
-            while (i != _equipmentIndex)
-            {
-                if (EquipmentSlotSet.Slots[i].Kind == slotKind)
-                    break;
-
-                i = i == 0 ? EquipmentSlotSet.Slots.Count - 1 : i - 1;
-            }
-
-            _equipmentIndex = i;
+            _equipmentSlotPane.ResetEquipping();
         }
 
         public override IInputListener Focus(Coord pos)
         {
-            IInputListener baseFocus = base.Focus(pos);
+            if (!HasFocus)
+            {
+                _inventoryPane.ResetSelectingItem();
+                _equipmentSlotPane.ResetEquipping();
+            }
 
-            _equipmentIndex = -1;
-            _inventoryIndex = 0;
-
-            Dirty = true;
-            return baseFocus;
+            return base.Focus(pos);
         }
 
         public override void LoseFocus()
         {
             base.LoseFocus();
-            Dirty = true;
+        }
+
+        class EquipmentSlotPane : Pane
+        {
+            public EquipmentSlotSet EquipmentSlotSet { get; }
+
+            public bool CurrentlyEquipping => _currentlyEquipping != null;
+
+            private int _equipmentIndex;
+            private Item _currentlyEquipping;
+
+            public EquipmentSlotPane(EquipmentSlotSet equipmentSlotSet)
+            {
+                EquipmentSlotSet = equipmentSlotSet;
+                equipmentSlotSet.OnEquipped += OnEquipped;
+                equipmentSlotSet.OnUnequipped += OnUnEquipped;
+
+                _equipmentIndex = -1;
+                _currentlyEquipping = null;
+
+                Dirty = true;
+            }
+
+            private void OnEquipped(EquipmentSlot arg1, Item arg2)
+            {
+                Dirty = true;
+            }
+
+            private void OnUnEquipped(EquipmentSlot arg1, Item arg2)
+            {
+                Dirty = true;
+            }
+
+            public override bool Paint(IWriteable writeContext)
+            {
+                if (!Dirty)
+                    return false;
+
+                Coord nextMessage = new Coord(0, 0);
+                writeContext.Clear();
+
+                Coord equippedLineEnd = StringPrinter.Print("Equipped:", writeContext, nextMessage.X, nextMessage.Y);
+                nextMessage = new Coord(0, equippedLineEnd.Y + 1);
+
+                foreach ((EquipmentSlot equipmentSlot, int i) in EquipmentSlotSet.Slots.WithIndex())
+                {
+                    bool slotSelected = _equipmentIndex == i;
+                    Color textColor = slotSelected ? Color.Black : Color.White;
+                    Color backgroundColor = slotSelected ? Color.White : Color.Black;
+
+                    Coord lineEnd = StringPrinter.Print($"{equipmentSlot.Name}: {EquipmentSlotSet.Equipped.GetValueOrDefault(equipmentSlot)?.Title.Name ?? "Nothing"}", writeContext, textColor, backgroundColor, nextMessage.X, nextMessage.Y);
+
+                    nextMessage = new Coord(0, lineEnd.Y + 1);
+                }
+
+                Dirty = false;
+
+                return true;
+            }
+
+            public void ResetEquipping()
+            {
+                _currentlyEquipping = null;
+                _equipmentIndex = -1;
+
+                Dirty = true;
+            }
+
+            public void BeginEquipping(Item item)
+            {
+                if (!item.CanBeEquipped)
+                    return;
+
+                _currentlyEquipping = item;
+                IncrementEquipmentIndex();
+            }
+
+            public EquipmentSlot GetCurrentlySelectedSlot()
+            {
+                if (_currentlyEquipping == null)
+                    return null;
+
+                return EquipmentSlotSet.Slots[_equipmentIndex];
+            }
+
+            public void IncrementEquipmentIndex()
+            {
+                EquipmentSlotKind slotKind = _currentlyEquipping.SlotKind;
+
+                int i = (_equipmentIndex + 1) % EquipmentSlotSet.Slots.Count;
+
+                while (i != _equipmentIndex)
+                {
+                    if (EquipmentSlotSet.Slots[i].Kind == slotKind)
+                        break;
+
+                    i = (i + 1) % EquipmentSlotSet.Slots.Count;
+                }
+
+                _equipmentIndex = i;
+
+                Dirty = true;
+            }
+
+            public void DecrementEquipmentIndex()
+            {
+                EquipmentSlotKind slotKind = _currentlyEquipping.SlotKind;
+                int i = _equipmentIndex == 0 ? EquipmentSlotSet.Slots.Count - 1 : _equipmentIndex - 1;
+
+                while (i != _equipmentIndex)
+                {
+                    if (EquipmentSlotSet.Slots[i].Kind == slotKind)
+                        break;
+
+                    i = i == 0 ? EquipmentSlotSet.Slots.Count - 1 : i - 1;
+                }
+
+                _equipmentIndex = i;
+
+                Dirty = true;
+            }
+        }
+
+        class InventoryPane : Pane
+        {
+            public Inventory Inventory { get; }
+
+            public int SelectedItemIndex { get; protected set; }
+
+            public Item CurrentlySelectedItem => SelectedItemIndex != -1 ? Inventory.Items.Keys.AtIndex(SelectedItemIndex) : null;
+
+            public InventoryPane(Inventory inventory)
+            {
+                Inventory = inventory;
+                SelectedItemIndex = -1;
+
+                Dirty = true;
+                Inventory.OnAdd += InventoryOnAdd;
+                Inventory.OnRemove += InventoryOnRemove;
+
+            }
+            private void InventoryOnAdd(Item obj)
+            {
+                Dirty = true;
+            }
+
+            private void InventoryOnRemove(Item obj)
+            {
+                Dirty = true;
+            }
+
+            public void ResetSelectingItem()
+            {
+                SelectedItemIndex = 0;
+                Dirty = true;
+            }
+
+            public void IncrementSelectedItemIndex()
+            {
+                SelectedItemIndex = Math.Min(SelectedItemIndex + 1, Inventory.Items.Count - 1);
+                Dirty = true;
+            }
+
+            public void DecrementSelectedItemIndex()
+            {
+                SelectedItemIndex = Math.Max(SelectedItemIndex - 1, 0);
+                Dirty = true;
+            }
+
+            public override bool Paint(IWriteable writeContext)
+            {
+                if (!Dirty)
+                    return false;
+
+                Coord nextMessage = new Coord(0, 0);
+
+                Coord inventoryLineEnd = StringPrinter.Print("Inventory:", writeContext, nextMessage.X, nextMessage.Y);
+                nextMessage = new Coord(0, inventoryLineEnd.Y + 1);
+
+                foreach ((Item item, int i) in Inventory.Items.Keys.WithIndex())
+                {
+                    bool itemSelected = i == SelectedItemIndex;
+                    Color textColor = itemSelected ? Color.Black : Color.White;
+                    Color backgroundColor = itemSelected ? Color.White : Color.Black;
+
+                    Coord lineEnd = StringPrinter.Print($"{item.Title.Name} (x{Inventory.Items[item]})", writeContext, textColor, backgroundColor, nextMessage.X, nextMessage.Y);
+
+                    nextMessage = new Coord(0, lineEnd.Y + 1);
+                }
+
+                Dirty = false;
+
+                return true;
+            }
         }
     }
 }
