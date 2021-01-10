a34ba1a7190ca66e6ed08d80e7eca1272ad2dcad
Drew Cutchins <drew.cutch@gmail.com>
2021-01-10 01:22:27 -0500

    Create BiomeBuilder to provide fluent interface for creating Biomes

---
+++ SpellSword/MapGeneration/BiomeBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace SpellSword.MapGeneration
+{
+    class BiomeBuilder
+    {
+        private readonly List<(IAreaDecorator, int)> _decoratorWeights;
+
+        public BiomeBuilder()
+        {
+            _decoratorWeights = new List<(IAreaDecorator, int)>();
+        }
+
+        public BiomeBuilder WithDecorator(IAreaDecorator decorator, int weight)
+        {
+            _decoratorWeights.Add((decorator, weight));
+            return this;
+        }
+
+        public Biome GetBiome()
+        {
+            WeightedRandomBag<IAreaDecorator> decorators = new WeightedRandomBag<IAreaDecorator>(_decoratorWeights);
+            return new Biome(decorators);
+        }
+    }
+}
