e0c7c3c872fc23283cc219fc6f957fa314a4a21e
Drew Cutchins <drew.cutch@gmail.com>
2021-01-09 19:52:41 -0500

    Update BearTermInputRouter to route input to window

--- SpellSword/Input/BearTermInputRouter.cs
+++ SpellSword/Input/BearTermInputRouter.cs
@@ -1,25 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using BearLib;
 using GoRogue;
 using GoRogue.Messaging;
+using SpellSword.Engine;
+using SpellSword.Render.Panes;
+using SpellSword.Util;
 
 namespace SpellSword.Input
 {
-    class BearTermInputRouter
+    class BearTermInputRouter: ISubscriber<WindowEvent>
     {
-        private readonly IInputListener _listener;
+        //private IInputListener _listener => _windowListeners.Peek();
+
+
+        private OrderedDictionary<PaneBounds, IInputListener> _windowListeners;
 
         private IInputListener _focus;
 
         private Coord _mouseLast;
 
-        public BearTermInputRouter(IInputListener listener)
         {
             _listener = listener;
             _focus = listener;
             _mouseLast = MousePos();
+            _windowListeners = new OrderedDictionary<PaneBounds, IInputListener>();
         }
 
         public void HandleInput()
@@ -30,7 +37,10 @@ namespace SpellSword.Input
 
                 if (input == Terminal.TK_MOUSE_LEFT)
                 {
-                    IInputListener newFocus = _listener.Focus(MousePos());
+                    PaneBounds target = GetTopPaneBounds(MousePos());
+                    IInputListener newFocus = target.Pane.Focus(MousePos());
+
+                    _windowListeners[target] = newFocus;
 
                     if (newFocus != _focus)
                     {
@@ -38,11 +48,11 @@ namespace SpellSword.Input
                         _focus = newFocus;
                     }
 
-                    _listener.OnMouseClick(MousePos()); 
+                    target.Pane.OnMouseClick(MousePos());
                 }
                 else if (input == Terminal.TK_MOUSE_MOVE)
                 {
-                    _listener.OnMouseMove(_mouseLast, MousePos());
+                    _windowListeners.Values.Last().OnMouseMove(_mouseLast, MousePos());
                     _mouseLast = MousePos();
                 }
                 else if (input < Terminal.TK_MOUSE_LEFT) // All key presses
@@ -58,6 +68,70 @@ namespace SpellSword.Input
         private Coord MousePos()
         {
             return new Coord(Terminal.State(Terminal.TK_MOUSE_X), Terminal.State(Terminal.TK_MOUSE_Y));
+        }
+
+        public void Handle(WindowEvent message)
+        {
+            if (message.OpenEvent)
+            {
+                _focus = message.Root.Focus(new Coord(0, 0));
+
+                _windowListeners.Add(new PaneBounds(message.Root, message.Bounds), _focus);
+            }
+            else
+            {
+                _windowListeners.Remove(new PaneBounds(message.Root, message.Bounds));
+
+                IInputListener newFocus = _windowListeners.Values.Last();
+
+                if(_focus != newFocus)
+                    _focus.LoseFocus();
+                
+                _focus = _windowListeners.Values.Last();
+            }
+        }
+
+        private PaneBounds GetTopPaneBounds(Coord pos)
+        {
+            foreach (PaneBounds paneBounds in _windowListeners.Keys.Reverse())
+            {
+                if (!paneBounds.Bounds.Contains(pos))
+                    continue;
+
+                return paneBounds;
+            }
+
+            return null;
+        }
+
+        private class PaneBounds
+        {
+            public Pane Pane { get; }
+            public Rectangle Bounds { get; }
+
+            public PaneBounds(Pane pane, Rectangle bounds)
+            {
+                Pane = pane;
+                Bounds = bounds;
+            }
+
+            protected bool Equals(PaneBounds other)
+            {
+                return Equals(Pane, other.Pane);
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((PaneBounds)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return Pane.GetHashCode();
+            }
         }
     }
 }
