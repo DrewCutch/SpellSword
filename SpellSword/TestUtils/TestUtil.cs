cdccb0205a5e07e941462815b77b9becead2ef95
Drew Cutchins <drew.cutch@gmail.com>
2021-01-09 20:02:58 -0500

    Update CreateGoblin in TestUtil to hold gold and recieve effects

--- SpellSword/TestUtils/TestUtil.cs
+++ SpellSword/TestUtils/TestUtil.cs
@@ -23,6 +23,9 @@ namespace SpellSword.TestUtils
     {
         private static Alignment goblins = new Alignment("goblins", AlignmentRelation.Enemy);
 
+        private static Item gold = new Item(new Title("", "gold coin"), "shiny gold coin", new Glyph('$', Color.Gold),
+            EquipmentSlotKind.None, 1);
+
         public static GameObject CreateGoblin(Coord pos, Actor player, Timeline timeline, GoalMapStore goalMapStore, MessageBus mainBus) { 
             GameObject goblin = new GameObject(pos, Layers.Main, null, isWalkable:true);
 
@@ -31,12 +34,16 @@ namespace SpellSword.TestUtils
             Being goblinBeing = new Being(new SelectedAttributes(new AttributeSet(6, 15, 4, 4, 3)), goblins, new EquipmentSlotSet(), 4, "goblin");
 
             goblinBeing.Equipment.Equip(mainWeapon, EquipmentSlot.RightHandEquip);
+            goblinBeing.Inventory.Add(gold, 10);
 
             Actor goblinActor = new Actor(goblinBeing, new BasicAgent(player, goalMapStore), mainBus);
             goblin.AddComponent(goblinActor);
             goblin.AddComponent(new GlyphComponent(new Glyph('g', Color.ForestGreen)));
-
             goblin.AddComponent(new NameComponent(new Title("a","goblin")));
+
+            EffectTargetComponent effectTarget = new EffectTargetComponent();
+            effectTarget.EffectTarget.AddEffectReceiver(goblinActor);
+            goblin.AddComponent(effectTarget);
 
             timeline.OnAdvance += goblinActor.Update;
 
