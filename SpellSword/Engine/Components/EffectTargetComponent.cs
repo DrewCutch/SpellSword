fc05dc722b1ca900a50267b65063af06b89eb5d0
Drew Cutchins <drew.cutch@gmail.com>
2021-01-09 19:59:38 -0500

    Create EffectTargetComponent to attach EffectTargets to GameObjects

---
+++ SpellSword/Engine/Components/EffectTargetComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SpellSword.Actors.Effects;
+using SpellSword.Update;
+
+namespace SpellSword.Engine.Components
+{
+    class EffectTargetComponent: Component, IUpdate
+    {
+        public EffectTarget EffectTarget { get; }
+        public EffectTargetComponent()
+        {
+            EffectTarget = new EffectTarget();
+        }
+
+        public void Update(int ticks)
+        {
+            EffectTarget.UpdateActiveEffects(ticks);
+        }
+    }
+}
