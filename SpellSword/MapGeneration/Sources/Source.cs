e25e8ab52378cf30c326bbf1a8dac2fb76910756
Drew Cutchins <drew.cutch@gmail.com>
2021-01-15 22:32:03 -0500

    Create Source for modeling sources of procedurally generated prefab options

---
+++ SpellSword/MapGeneration/Sources/Source.cs
@@ -0,0 +1,46 @@
+﻿using Troschuetz.Random;
+
+namespace SpellSword.MapGeneration.Sources
+{
+    public static class Source
+    {
+        public static Source<T> From<T>(T value)
+        {
+            return new ConstantSource<T>(value);
+        }
+    }
+
+    public abstract class Source<T>
+    {
+        public bool Shared { get; }
+
+        protected Source(bool shared)
+        {
+            Shared = shared;
+        }
+
+        public abstract SourceCursor<T> Pull(IGenerator rng);
+        public abstract bool IsEmpty();
+
+        public abstract Source<T> Clone();
+
+        public static implicit operator Source<T>(T value)
+        {
+            return new ConstantSource<T>(value);
+        }
+    }
+}
+
+/*
+    WeightedSource specialRoomsWeighted = new WeightedSource();
+    specialRooms.Add(temple);
+    specialRooms.Add(throneRoom);
+    specialRooms.Add(jailRoom);
+
+    LimitedSource specialRooms = new LimitedSource(specialRoomsWeighted, 1);
+
+    WeightedSource roomNeighbors = new WeightedSource();
+    roomNeighbors.Add(specialRooms, 1);
+    roomNeighbors.Add(basicRoom, 10);
+    
+*/
---
+++ SpellSword/MapGeneration/Sources/SourceCursor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpellSword.MapGeneration.Sources
+{
+    public class SourceCursor<T>
+    {
+        public readonly T Value;
+
+        public event Action<T> OnUsed;
+
+        public SourceCursor(T value, Action<T> onUsed)
+        {
+            Value = value;
+            OnUsed += onUsed;
+        }
+
+        public T Use()
+        {
+            OnUsed.Invoke(Value);
+
+            return Value;
+        }
+    }
+}
