129cfacfaf3e45c92778a4f4a6017b305148c752
Drew Cutchins <drew.cutch@gmail.com>
2021-01-15 22:36:45 -0500

    Update Biomes to use new generation features

--- SpellSword/MapGeneration/Resources/Biomes.cs
+++ SpellSword/MapGeneration/Resources/Biomes.cs
@@ -4,10 +4,16 @@ using System.Drawing;
 using System.Text;
 using GoRogue;
 using GoRogue.GameFramework;
+using GoRogue.MapViews;
+using GoRogue.Random;
 using SpellSword.Engine;
 using SpellSword.Engine.Components;
+using SpellSword.MapGeneration.Sources;
+using SpellSword.MapGeneration.Structure;
+using SpellSword.Render;
 using SpellSword.Render.Lighting;
 using SpellSword.Speech;
+using Rectangle = GoRogue.Rectangle;
 
 namespace SpellSword.MapGeneration.Resources
 {
@@ -24,21 +30,150 @@ namespace SpellSword.MapGeneration.Resources
             },
             100, .7f, .2f);
 
+        
+        private static Spreader TallGrassSpreader = new Spreader(
+            (mapInfo, pos) =>
+            {
+                GameObject grass = new GameObject(pos, Layers.Floor, null, isTransparent:false);
+                grass.AddComponent(new GlyphComponent('⌠', Color.DarkGreen));
+                grass.AddComponent(new NameComponent(new Title("in", "", "tall grass")));
+
+                return grass;
+            },
+            50, .7f, .1f);
+
+        
         private static Spreader GlowingMushroomsSpreader = new Spreader(
             (mapInfo, pos) =>
             {
                 GameObject grass = new GameObject(pos, Layers.Floor, null);
                 grass.AddComponent(new GlyphComponent('"', Color.Cyan));
-                grass.AddComponent(new LightSourceComponent(mapInfo.LightMap, new Light(Color.Cyan, Coord.NONE, 3, 5)));
-                grass.AddComponent(new NameComponent(new Title("in", "", "grass")));
+                grass.AddComponent(new LightSourceComponent(mapInfo.LightMap, new Light(Color.Cyan, new Coord(0,0), 3, 5)));
+                grass.AddComponent(new NameComponent(new Title("in", "", "mana mushrooms")));
 
                 return grass;
-            }, 20, .8f, .8f);
+            }, 15, .8f, .8f);
+
+       
+        private static Placable TorchPlacer = new Placable(
+            (mapInfo, pos) =>
+        {
+            Coord wallAway = new Coord(0, 0);
+
+            foreach (Direction dir in AdjacencyRule.CARDINALS.DirectionsOfNeighbors())
+            {
+                if (mapInfo.Map.Contains(pos + dir) && !(mapInfo.Map.Terrain[pos + dir]?.IsWalkable ?? false))
+                {
+                    wallAway -= new Coord(dir.DeltaX, dir.DeltaY);
+                }
+            }
+
+            if (wallAway.X == 0 && wallAway.Y == 0)
+            {
+                foreach (Direction dir in AdjacencyRule.DIAGONALS.DirectionsOfNeighbors())
+                {
+                    if (mapInfo.Map.Contains(pos + dir) && !(mapInfo.Map.Terrain[pos + dir]?.IsWalkable ?? false))
+                    {
+                        wallAway -= new Coord(dir.DeltaX, dir.DeltaY);
+                    }
+                }
+            }
+
+            GameObject torch = new GameObject(pos, Layers.Main, null);
+            torch.AddComponent(new GlyphComponent('i', Color.OrangeRed));
+            torch.AddComponent(new LightSourceComponent(mapInfo.LightMap, new Light(Color.LightGoldenrodYellow, wallAway, 20, 20)));
+            torch.AddComponent(new NameComponent(new Title("over", "", "torch")));
+
+            return torch;
+        });
+
+        private static IPlaceable WallPlaceable = new Placable(
+            (info, pos) =>
+            {
+                IGameObject wall = new GameObject(pos, Layers.Terrain, null, true, false, false);
+                wall.AddComponent(new GlyphComponent(new Glyph('#', Color.DimGray, Color.DimGray)));
+                wall.AddComponent(new NameComponent(new Title("a", "stone wall")));
+
+                return wall;
+            });
+
+        private static IPlaceable FloorPlacable = new Placable(
+            (info, pos) =>
+            {
+                IGameObject floor = new GameObject(pos, Layers.Terrain, null, true);
+
+                int red = SingletonRandom.DefaultRNG.Next(85, 95);
+                int green = SingletonRandom.DefaultRNG.Next(55, 65);
+                int blue = SingletonRandom.DefaultRNG.Next(15, 25);
+                Color tileColor = Color.FromArgb(green, green, green);
+
+                GlyphComponent glyphComponent = new GlyphComponent(new Glyph(' ', tileColor, tileColor));
+                floor.AddComponent(glyphComponent);
+                floor.AddComponent(new DecalComponent());
+                floor.AddComponent(new NameComponent(new Title("a", "dirt floor")));
+
+                return floor;
+            });
+
+        private static Source<IRoomGenerator> RootSource()
+        {
+            IRoomGenerator closet = new RectRoomGenerator(new Rectangle(0, 0, 4, 4), new Rectangle(0, 0, 6, 6), WallPlaceable, FloorPlacable);
+            IRoomGenerator basicRoom = new RectRoomGenerator(new Rectangle(0, 0, 8, 8), new Rectangle(0, 0, 10, 10), WallPlaceable, FloorPlacable);
+            IRoomGenerator largeRoom = new RectRoomGenerator(new Rectangle(0, 0, 10, 10), new Rectangle(0, 0, 20, 20), WallPlaceable, FloorPlacable);
+
+            IRoomGenerator hugeRoom = new RectRoomGenerator(new Rectangle(0, 0, 20, 20), new Rectangle(0, 0, 30, 30), WallPlaceable, FloorPlacable);
+
+            WeightedSource<IRoomGenerator> roomSource = new WeightedSource<IRoomGenerator>(true);
+            roomSource.Add(Source.From(closet), 1);
+            roomSource.Add(Source.From(basicRoom), 5);
+            roomSource.Add(Source.From(largeRoom), 5);
+            roomSource.Add(new LimitedSource<IRoomGenerator>(Source.From(hugeRoom), 1, true), 3);
+
+            basicRoom.NeighborPossibilities = roomSource;
+            largeRoom.NeighborPossibilities = roomSource;
+            hugeRoom.NeighborPossibilities = roomSource;
+
+            WeightedSource<IAreaDecorator> lightingSource = new WeightedSource<IAreaDecorator>(true);
+            lightingSource.Add(Source.From<IAreaDecorator>(new PlaceableDecorator(GlowingMushroomsSpreader)), 1);
+            lightingSource.Add(Source.From<IAreaDecorator>(new WallDecorator(TorchPlacer)), 5);
+
+
+            PrioritySource<IAreaDecorator> decorationSource = new PrioritySource<IAreaDecorator>(false);
+            
+            // First, place lighting in room (always)
+            decorationSource.Add(new LimitedSource<IAreaDecorator>(lightingSource, 1, false));
+
+            //Then place foliage
+            WeightedSource<IAreaDecorator> foliageSource = new WeightedSource<IAreaDecorator>(true);
+            foliageSource.Add(Source.From<IAreaDecorator>(new PlaceableDecorator(GrassSpreader)), 1);
+            foliageSource.Add(Source.From<IAreaDecorator>(new PlaceableDecorator(TallGrassSpreader)), 1);
+
+            //decorationSource.Add(foliageSource);
+
+            basicRoom.Decorators = decorationSource;
+            largeRoom.Decorators = decorationSource;
+
+            return Source.From(basicRoom);
+        }
+
+        private static Source<IHallGenerator> HallSource()
+        {
+            IHallGenerator hallGenerator = new HallGenerator(WallPlaceable, FloorPlacable, null);
+
+            return Source.From(hallGenerator);
+        }
+
+
+
 
         public static Biome TestBiome = 
             new BiomeBuilder()
                 .WithDecorator(new PlaceableDecorator(GrassSpreader), 2)
                 .WithDecorator(new PlaceableDecorator(GlowingMushroomsSpreader), 1)
+                .WithDecorator(new PlaceableDecorator(TallGrassSpreader), 1)
+                .WithDecorator(new WallDecorator(TorchPlacer), 3)
+                .WithRoomSource(RootSource())
+                .WithHallSource(HallSource())
                 .GetBiome();
 
 
