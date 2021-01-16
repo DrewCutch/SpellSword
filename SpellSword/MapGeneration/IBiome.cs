32d3c84b47d61936fcf4541098db5cce3bd2cec8
Drew Cutchins <drew.cutch@gmail.com>
2021-01-15 22:31:07 -0500

    Update IBiome to use GenerateOn and Populate methods

--- SpellSword/MapGeneration/IBiome.cs
+++ SpellSword/MapGeneration/IBiome.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GoRogue;
 using GoRogue.GameFramework;
 using GoRogue.MapGeneration;
 using GoRogue.MapViews;
+using SpellSword.Game;
+using SpellSword.Util;
 using Troschuetz.Random;
 
 namespace SpellSword.MapGeneration
 {
     interface IBiome
     {
-        public IMapView<GameObject> GenerateTerrain(IGenerator rng);
-
-        public void Populate(MapInfo mapInfo, MapArea area, IGenerator rng);
+        public IEnumerable<MapInfo> GenerateOn(Floor floor, Rectangle area, ResettableRandom rng);
+        public void Populate(Floor floor, MapArea area, IGenerator rng);
     }
 }
