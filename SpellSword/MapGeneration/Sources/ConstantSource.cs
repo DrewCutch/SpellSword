164aea04e092f49bda2ce1d384ca613d3716db6f
Drew Cutchins <drew.cutch@gmail.com>
2021-01-15 22:32:35 -0500

    Create ConstantSource

---
+++ SpellSword/MapGeneration/Sources/ConstantSource.cs
@@ -0,0 +1,28 @@
+﻿using Troschuetz.Random;
+
+namespace SpellSword.MapGeneration.Sources
+{
+    class ConstantSource<T>: Source<T>
+    {
+        private T _value;
+        public ConstantSource(T value) : base(true)
+        {
+            _value = value;
+        }
+
+        public override SourceCursor<T> Pull(IGenerator rng)
+        {
+            return new SourceCursor<T>(_value, (val) => { });
+        }
+
+        public override bool IsEmpty()
+        {
+            return false;
+        }
+
+        public override Source<T> Clone()
+        {
+            return this;
+        }
+    }
+}
