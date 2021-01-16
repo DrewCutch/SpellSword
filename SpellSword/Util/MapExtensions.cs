bd0676dffc5c4b7c080a5fe1b1572e0c2a253877
Drew Cutchins <drew.cutch@gmail.com>
2021-01-15 22:22:41 -0500

    Create MapExtensions with AllEmpty method

---
+++ SpellSword/Util/MapExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GoRogue;
+using GoRogue.GameFramework;
+using GoRogue.MapViews;
+
+namespace SpellSword.Util
+{
+    static class MapExtensions
+    {
+        public static bool AllEmpty(this IMapView<IGameObject> map, Rectangle area)
+        {
+            foreach (Coord pos in area.Positions())
+            {
+                if (!map.Contains(pos) || map[pos] != null)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
