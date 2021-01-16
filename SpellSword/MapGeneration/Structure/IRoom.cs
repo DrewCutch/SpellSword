f9801c727872431c1e928373d6a64a510b365791
Drew Cutchins <drew.cutch@gmail.com>
2021-01-15 22:33:37 -0500

    Create IRoom

---
+++ SpellSword/MapGeneration/Structure/IRoom.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using GoRogue;
+using GoRogue.MapGeneration;
+
+namespace SpellSword.MapGeneration.Structure
+{
+    interface IRoom
+    {
+        public IRoomGenerator GeneratedBy { get; }
+
+        public IReadOnlyList<RoomConnection> PotentialConnections { get; }
+        public int OpenConnections { get; }
+        public MapArea Area { get; }
+
+        public bool UseConnection(RoomConnection connection);
+    }
+}
