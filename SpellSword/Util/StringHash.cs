c5cf4364695bac5ed90c61a29faf5e46a0aa3bb4
Drew Cutchins <drew.cutch@gmail.com>
2021-01-15 22:22:11 -0500

    Create StringHash.StableHash for stable string hashing (used for rng seed)

---
+++ SpellSword/Util/StringHash.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpellSword.Util
+{
+    static class StringHash
+    {
+        public static int StableHash(string str)
+        {
+            int hash = 5381;
+
+            foreach (char c in str)
+                hash = ((hash << 5) + hash) + c;
+
+            return hash;
+        }
+    }
+}
