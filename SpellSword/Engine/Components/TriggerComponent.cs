de692aac50b43785cac261a9b4257e3dbcf2f958
Drew Cutchins <drew.cutch@gmail.com>
2021-01-15 22:37:44 -0500

    Create TriggerComponent with TriggerRouter

---
+++ SpellSword/Engine/Components/TriggerComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GoRogue;
+using GoRogue.GameFramework;
+
+namespace SpellSword.Engine.Components
+{
+    class TriggerComponent: Component
+    {
+        private Action<IGameObject, Direction> _onEnter;
+
+        private Action<IGameObject, Direction> _onExit;
+
+        public TriggerComponent(Action<IGameObject, Direction> onEnter, Action<IGameObject, Direction> onExit)
+        {
+            _onEnter = onEnter;
+            _onExit = onExit;
+        }
+
+        public void Enter(IGameObject gameObject, Direction direction)
+        {
+            _onEnter.Invoke(gameObject, direction);
+        }
+
+        public void Exit(IGameObject gameObject, Direction direction)
+        {
+            _onExit.Invoke(gameObject, direction);
+        }
+    }
+}
---
+++ SpellSword/Engine/TriggerRouter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GoRogue;
+using GoRogue.GameFramework;
+using SpellSword.Engine.Components;
+
+namespace SpellSword.Engine
+{
+    class TriggerRouter
+    {
+        public Map Map { get; }
+
+        public TriggerRouter(Map map)
+        {
+            map.ObjectMoved += MapOnObjectMoved;
+
+            Map = map;
+        }
+
+        private void MapOnObjectMoved(object? sender, ItemMovedEventArgs<IGameObject> e)
+        {
+            Direction direction = Direction.GetDirection(e.OldPosition, e.NewPosition);
+
+            foreach (IGameObject gameObject in Map.GetObjects(e.OldPosition))
+            {
+                if (gameObject.GetComponent<TriggerComponent>() is TriggerComponent triggerComponent)
+                {
+                    triggerComponent.Exit(e.Item, direction);
+                }
+            }
+
+            foreach (IGameObject gameObject in Map.GetObjects(e.NewPosition))
+            {
+                if (gameObject.GetComponent<TriggerComponent>() is TriggerComponent triggerComponent)
+                {
+                    triggerComponent.Enter(e.Item, direction);
+                }
+            }
+            
+        }
+    }
+}
