295e45f0dab5b3797865889d5fe071c66dfc9817
Drew Cutchins <drew.cutch@gmail.com>
2021-01-15 22:33:45 -0500

    Create BasicRoom

---
+++ SpellSword/MapGeneration/Structure/BasicRoom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GoRogue;
+using GoRogue.MapGeneration;
+
+namespace SpellSword.MapGeneration.Structure
+{
+    class BasicRoom: IRoom
+    {
+        private readonly List<RoomConnection> _potentialConnections;
+        public IRoomGenerator GeneratedBy { get; }
+        public IReadOnlyList<RoomConnection> PotentialConnections => _potentialConnections;
+        public int OpenConnections => PotentialConnections.Count;
+        public MapArea Area { get; }
+
+        public BasicRoom(MapArea area, List<RoomConnection> potentialConnections, IRoomGenerator generatedBy)
+        {
+            Area = area;
+            _potentialConnections = potentialConnections;
+            GeneratedBy = generatedBy;
+        }
+
+        public bool UseConnection(RoomConnection connection)
+        {
+            return _potentialConnections.Remove(connection);
+        }
+    }
+}
