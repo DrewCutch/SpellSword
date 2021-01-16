df088c0ca79a668a177e97aae4d3677f2ee53dbf
Drew Cutchins <drew.cutch@gmail.com>
2021-01-15 22:33:56 -0500

    Create IHallGenerator interface

---
+++ SpellSword/MapGeneration/Structure/IHallGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GoRogue;
+using Troschuetz.Random;
+
+namespace SpellSword.MapGeneration.Structure
+{
+    interface IHallGenerator
+    {
+        public IRoom Generate(MapInfo mapInfo, Coord connectFrom, Coord connectTo, IGenerator rng);
+
+        public bool CanGenerate(MapInfo mapInfo, Coord connectFrom, Coord connectTo, IGenerator rng);
+    }
+}
