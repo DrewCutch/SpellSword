e547af57132564ec63e95a897fcf13c63101fad1
Drew Cutchins <drew.cutch@gmail.com>
2021-01-15 22:33:22 -0500

    Create IRoomGenerator interface

---
+++ SpellSword/MapGeneration/Structure/IRoomGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using GoRogue;
+using SpellSword.MapGeneration.Decorators;
+using SpellSword.MapGeneration.Sources;
+using Troschuetz.Random;
+
+namespace SpellSword.MapGeneration.Structure
+{
+    interface IRoomGenerator
+    {
+        Source<IRoomGenerator> NeighborPossibilities { get; set; }
+
+        Source<IAreaDecorator> Decorators { get; set; }
+
+        public IRoom Generate(MapInfo mapInfo, RoomConnection connectAt, IGenerator rng);
+
+        public bool CanGenerate(MapInfo mapInfo, RoomConnection connectAt, IGenerator rng);
+
+        public List<Hook> Populate(MapInfo mapInfo, IRoom room, IGenerator rng);
+    }
+}
---
+++ SpellSword/MapGeneration/Structure/RoomConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GoRogue;
+using SpellSword.MapGeneration.Sources;
+
+namespace SpellSword.MapGeneration.Structure
+{
+    class RoomConnection
+    {
+        public Coord Position { get; }
+        public Direction Direction { get; }
+
+        public Source<IRoomGenerator> Possibilities { get; }
+
+        public bool UseHall { get; }
+
+        public RoomConnection(Coord position, Direction direction, Source<IRoomGenerator> possibilities, bool useHall)
+        {
+            Position = position;
+            Direction = direction;
+            Possibilities = possibilities;
+            UseHall = useHall;
+        }
+    }
+}
