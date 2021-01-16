9e45aff2ffe5662e94493fe42368783ce2a12cd4
Drew Cutchins <drew.cutch@gmail.com>
2021-01-15 22:36:33 -0500

    Update BiomeBuilder to support new Biome features

--- SpellSword/MapGeneration/BiomeBuilder.cs
+++ SpellSword/MapGeneration/BiomeBuilder.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using SpellSword.MapGeneration.Sources;
+using SpellSword.MapGeneration.Structure;
 
 namespace SpellSword.MapGeneration
 {
     class BiomeBuilder
     {
         private readonly List<(IAreaDecorator, int)> _decoratorWeights;
+
+        private Source<IRoomGenerator> _rootRoomSource;
+
+        private Source<IHallGenerator> _hallGeneratorSource;
 
         public BiomeBuilder()
         {
@@ -20,10 +26,24 @@ namespace SpellSword.MapGeneration
             return this;
         }
 
+        public BiomeBuilder WithRoomSource(Source<IRoomGenerator> rootRoomSource)
+        {
+            _rootRoomSource = rootRoomSource;
+
+            return this;
+        }
+
+        public BiomeBuilder WithHallSource(Source<IHallGenerator> hallSource)
+        {
+            _hallGeneratorSource = hallSource;
+
+            return this;
+        }
+
         public Biome GetBiome()
         {
             WeightedRandomBag<IAreaDecorator> decorators = new WeightedRandomBag<IAreaDecorator>(_decoratorWeights);
-            return new Biome(decorators);
+            return new Biome(decorators, _rootRoomSource, _hallGeneratorSource);
         }
     }
 }
