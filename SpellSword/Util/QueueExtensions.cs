8cdd388687fa26f69c61aee202ca4ae3c1c271fe
Drew Cutchins <drew.cutch@gmail.com>
2021-01-15 22:24:52 -0500

    Create QueueExtensions with EnqueueRange method

---
+++ SpellSword/Util/QueueExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpellSword.Util
+{
+    static class QueueExtensions
+    {
+        public static void EnqueueRange<T>(this Queue<T> queue, IEnumerable<T> elements)
+        {
+            foreach (T element in elements)
+            {
+                queue.Enqueue(element);
+            }
+        }
+    }
+}
