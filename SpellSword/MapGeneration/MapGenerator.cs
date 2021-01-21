2f014ef1f3b26683329233976a848f13c35d06b8
Drew Cutchins <drew.cutch@gmail.com>
2021-01-20 21:49:49 -0500

    Update MapGenerator to conform to new IBiome calling conventions

--- SpellSword/MapGeneration/MapGenerator.cs
+++ SpellSword/MapGeneration/MapGenerator.cs
@@ -5,15 +5,19 @@ using System.Linq;
 using System.Text;
 using GoRogue;
 using GoRogue.GameFramework;
+using GoRogue.Messaging;
 using GoRogue.Random;
+using SpellSword.Actors.Pathing;
 using SpellSword.Engine;
 using SpellSword.Engine.Components;
+using SpellSword.Game;
 using SpellSword.MapGeneration.Resources;
 using SpellSword.MapGeneration.Sources;
 using SpellSword.MapGeneration.Structure;
 using SpellSword.Render;
 using SpellSword.Render.Lighting;
 using SpellSword.Speech;
+using SpellSword.Time;
 using SpellSword.Util;
 using SpellSword.Util.Collections;
 using Troschuetz.Random;
@@ -26,8 +30,13 @@ namespace SpellSword.MapGeneration
     {
         private IBiome _biome = Biomes.TestBiome;
 
-        public MapGenerator()
+        private Timeline _timeline;
+        private MessageBus _messageBus;
+
+        public MapGenerator(Timeline timeline, MessageBus messageBus)
         {
+            _timeline = timeline;
+            _messageBus = messageBus;
         }
 
         public IEnumerable<MapInfo> GenerationSteps(int width, int height, string seed)
@@ -40,7 +49,7 @@ namespace SpellSword.MapGeneration
 
             ResettableRandom rng = new ResettableRandom(seed);
 
-            return _biome.GenerateOn(null, new Rectangle(0, 0, width, height), rng);
+            return _biome.GenerateOn(new Floor(0, _timeline, mapInfo, new Spawner(map), new GoalMapStore(map), _messageBus), new Rectangle(0, 0, width, height), rng);
         }
 
         public MapInfo Generate(int width, int height, string seed)
