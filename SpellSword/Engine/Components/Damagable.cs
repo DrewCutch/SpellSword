e4fff90ee060f01a7b2270614f08a36562dd7de4
Drew Cutchins <drew.cutch@gmail.com>
2021-02-11 18:26:02 -0800

    Create Damagable component

---
+++ SpellSword/Engine/Components/Damagable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpellSword.Engine.Components
+{
+    class Damagable: Component, IDamagable
+    {
+        public int CurrentHealth;
+        public int Health;
+        public Damagable(int health)
+        {
+            Health = health;
+            CurrentHealth = health;
+        }
+        public void DoDamage(Damage damage)
+        {
+            CurrentHealth -= damage.Amount;
+
+            if (CurrentHealth <= 0)
+                Parent.CurrentMap.RemoveEntity(Parent);
+        }
+    }
+}
